431a3a9b25c873ad65557a21cde41dc9547bebb3
vwcle <vwclenilson@outlook.com>
2022-11-21 21:55:29 -0300

    21/11/22-02

---
+++ 3-EstruturaDeRepeticao/11.dart
@@ -0,0 +1,16 @@
+/* Altere o programa anterior para mostrar no final a soma 
+dos números.  */
+
+import 'dart:io';
+
+void main() {
+  print('Digite um numero inteiro: ');
+  int a = int.parse(stdin.readLineSync()!);
+  print('Digite mais um numero inteiro: ');
+  int b = int.parse(stdin.readLineSync()!);
+  int soma = 0;
+  for (var i = a+1; i < b; i++) {
+    soma+=i;
+  }
+  print(soma);
+}
